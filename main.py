790276bdb098ebd5a14d088e0028fe0d44145040
vishalw-iitk <vishalwghmre88@gmail.com>
2019-11-19 01:36:44 +0530

    modified main.py and added new_file1.txt

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from hand_detection import hand_centre
 import cv2
 import os
 
-import numpy as np
+import pandas as pd
 
 main_folder = 'Train_data'
 save_folder = 'pipeline_train_data'
@@ -12,13 +12,13 @@ for folder in os.listdir(main_folder):
         os.mkdir(os.path.join(save_folder,folder))
         blank_image = np.zeros((260,210,3), np.uint8)
         temp = np.zeros((60,70,3), np.uint8)
-        for image_path in os.listdir(os.path.join(main_folder,folder)):
+         in os.listdir(os.path.join(main_folder,folder)):
             print(image_path)
             image_str = image_path.split('_')
             os.mkdir(os.path.join(save_folder,folder,image_str[4]))
             image = cv2.imread(os.path.join(main_folder,folder,image_path))
             face_crop = face_landmarks.detect(temp,image)
-            face_image_name = 'face_crop_'+image_path+'_'+'.png'
+            face_image_naav = 'face_crop_'+image_path+'_'+'.png'
             cv2.imwrite(os.path.join(save_folder,folder,image_str[4],face_image_name),face_crop)
             blank_image = hand_centre.detect(save_folder,folder,image,blank_image,image_str[4])
         flow_path = 'flow_'+image_path+'_'+'.png'   
